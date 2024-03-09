0f1246033541fec7367d3d565931b31005bd243c
bluesalt <siddhant.pandagle1998@gmail.com>
2024-03-09 16:03:21 +0000

    Main Page Canonical added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ import { GoogleTagManager } from '@next/third-parties/google'
 export const metadata: Metadata = {
   title: 'Congruence Market Insights - Market Research & Consulting',
   description: 'Congruence is your trusted partner in navigating emerging opportunities and challenges through high-quality, tailored market research reports.',
-  keywords: 'Congruence Market Insights, CMI, Market Research, Industry Analysis, Market Analysis, Global Market Trends, Business Intelligence, Custom Market Research, Strategic Insights, Market Dynamics, Emerging Opportunities, Competitive Landscape, Data-driven Decision Making, Market Forecast, Industry Reports, Tailored Research Solutions, Market Intelligence Services'
+  keywords: 'Congruence Market Insights, CMI, Market Research, Industry Analysis, Market Analysis, Global Market Trends, Business Intelligence, Custom Market Research, Strategic Insights, Market Dynamics, Emerging Opportunities, Competitive Landscape, Data-driven Decision Making, Market Forecast, Industry Reports, Tailored Research Solutions, Market Intelligence Services',
+  alternates: {
+    canonical: `https://www.congruencemarketinsights.com/`,
+  },
 }
 
 export default function RootLayout({
