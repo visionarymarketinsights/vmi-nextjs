7a729f17c5df3e738589eb318a18d1b5277f73e7
sidpandagle <adi.pandagle@gmail.com>
2024-03-18 22:01:18 +0530

    Local Temp Data added

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // export const apiUrl = 'http://localhost:8000'
-export const apiUrl = 'https://congapi.178765.xyz'
+export const apiUrl = 'https://railwayfastapi.onrender.com'
 
 export const reCaptchaKey = '6Ldr4ucoAAAAAKwW2FX-bCJIo6YHztxzf97Y_Tl8'
 
