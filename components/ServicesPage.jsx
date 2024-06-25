e8c61d7d3c742eddff305ff5c5ffb59cca1cee05
dark178765 <163676841+dark178765@users.noreply.github.com>
2024-06-25 14:13:10 +0530

    Services typos fixed

--- components/ServicesPage.jsx
+++ components/ServicesPage.jsx
@@ -27,22 +27,22 @@ export default function ServicesPage() {
                                     <svg className="flex-shrink-0 w-5 h-5 text-cyan-950 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
                                     <span className="text-base font-medium leading-tight text-gray-900 ">
 
-                                        Market forecast & demand-supply situatio</span>
+                                        Market forecast & demand-supply situation</span>
                                 </li>
                                 <li className="flex space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-cyan-950 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
                                     <span className="text-base font-medium leading-tight text-gray-900 ">
-                                        Drivers & Restraints of busines</span>
+                                        Drivers & Restraints of business</span>
                                 </li>
                                 <li className="flex space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-cyan-950 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
                                     <span className="text-base font-medium leading-tight text-gray-900 ">
-                                        Opportunity assessment using TAM – Total Addressable Marke</span>
+                                        Opportunity assessment using TAM – Total Addressable Market</span>
                                 </li>
                                 <li className="flex space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-cyan-950 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
                                     <span className="text-base font-medium leading-tight text-gray-900 ">
-                                        Key consumer trends & influential behaviors that drive product purchase,</span>
+                                        Key consumer trends & influential behaviors that drive product purchase</span>
                                 </li>
                                 <li className="flex space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-cyan-950 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
